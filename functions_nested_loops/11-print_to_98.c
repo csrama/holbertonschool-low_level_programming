c28722fb79ac6fd55f0bd07a11a1631b6e12f184
csrama <ramadafer1@gmail.com>
2025-10-14 13:18:04 +0000

    update

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,14 +1,60 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: The starting number
+ */
 void print_to_98(int n)
 {
-    while (n != 98)
+    int num;
+
+    if (n <= 98)
     {
-        printf("%d, ", n);
-        if (n > 98)
-            n--;
-        else
-            n++;
+        for (num = n; num <= 98; num++)
+        {
+            print_number(num);
+            if (num != 98)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
     }
-    printf("98\n");
+    else
+    {
+        for (num = n; num >= 98; num--)
+        {
+            print_number(num);
+            if (num != 98)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+    }
+    _putchar('\n');
+}
+
+/**
+ * print_number - Prints an integer using _putchar
+ * @n: The number to print
+ */
+void print_number(int n)
+{
+    unsigned int num;
+
+    if (n < 0)
+    {
+        _putchar('-');
+        num = -n;
+    }
+    else
+    {
+        num = n;
+    }
+
+    if (num / 10)
+        print_number(num / 10);
+
+    _putchar((num % 10) + '0');
 }
