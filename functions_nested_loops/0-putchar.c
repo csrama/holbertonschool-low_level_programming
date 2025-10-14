94e70238429d25c84ca05d7c33fc3e2b4a3a51c7
csrama <ramadafer1@gmail.com>
2025-10-14 09:14:32 +0000

    0 putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - prints Holberton
+ * main - Entry point
  *
- * Description: prints Holberton
- *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char text[10] = "Holberton";
-	int i = 0;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(text[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
