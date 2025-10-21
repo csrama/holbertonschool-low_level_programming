06b6c23667a02f8a5f4eaa81c7017ed02858e141
csrama <ramadafer1@gmail.com>
2025-10-21 08:20:16 +0000

    reset to 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
-void reset_to_98(int *n);
-
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-int num = 42;
-printf("Before: %d\n" num);
+	int num = 42;
 
-reset_to_98(&num);
+	printf("Before: %d\n", num);
+	reset_to_98(&num);
+	printf("After: %d\n", num);
 
-printf("After: %d\n", num);
-
-return 0;
+	return (0);
 }
