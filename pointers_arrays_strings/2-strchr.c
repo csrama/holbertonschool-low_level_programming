bfe7574b4e5d4f0c67ebe13b66575c15c5c723a9
csrama <ramadafer1@gmail.com>
2025-10-27 07:42:17 +0000

    Returns a pointer to the first occurrence of the character c in the string s, or NULL

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,22 +2,24 @@
 
 /**
  * _strchr - locates a character in a string
- * @s: string to search
- * @c: character to locate
+ * @s: the string to search
+ * @c: the character to find
  *
- * Return: pointer to first occurrence of c in s, or NULL if not found
+ * Return: pointer to the first occurrence of c in s,
+ * or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
-		{
 			return (s + i);
-		}
 	}
 
-	return (NULL);
+	if (s[i] == c)
+		return (s + i);
+
+	return (0);
 }
