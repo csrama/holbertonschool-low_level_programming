b0575994e4e9a7272171edc1a4f0b1d6c5adb5df
csrama <ramadafer1@gmail.com>
2025-10-28 07:14:12 +0000

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -3,21 +3,15 @@
 /**
  * _puts_recursion - Prints a string followed by a new line using recursion
  * @s: The string to be printed
- *
- * Return: void
  */
 void _puts_recursion(char *s)
 {
-    /* Base case: if we've reached the end of the string */
-    if (*s == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
-    
-    /* Print the current character */
-    _putchar(*s);
-    
-    /* Recursive call with the next character */
-    _puts_recursion(s + 1);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
