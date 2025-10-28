0394ca840a3572ddf5400a5402cf8c4b11c73fe1
csrama <ramadafer1@gmail.com>
2025-10-28 07:24:04 +0000

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string followed by a new line using recursion
+ * _puts_recursion - prints a string followed by a new line
  * @s: string to print
  *
- * Return: void
+ * Return: nothing
  */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		write(1, "\n", 1);
+		_putchar('\n');
 		return;
 	}
-
-	write(1, s, 1);
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
