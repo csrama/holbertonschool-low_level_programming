be7bdba134f81737536d3f7b3d38eb637c301860
csrama <ramadafer1@gmail.com>
2025-10-28 07:47:19 +0000

    5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,345 @@
+#include "main.h"
+
+/**
+ * _sqrt_helper - helper function to find square root using binary search
+ * @n: number to find square root of
+ * @guess: current guess for square root
+ *
+ * Return: natural square root of n, or -1 if not found
+ */
+int _sqrt_helper(int n, int guess)
+{
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+
+	if (guess * guess > n)
+	{
+		return (-1);
+	}
+
+	return (_sqrt_helper(n, guess + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to find square root of
+ *
+ * Return: natural square root of n, or -1 if not found
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+
+	return (_sqrt_helper(n, 1));
+}#include "main.h"
+
+/**
+ *  * _sqrt_helper - helper function to find square root using binary search
+ *   * @n: number to find square root of
+ *    * @guess: current guess for square root
+ *     *
+ *      * Return: natural square root of n, or -1 if not found
+ *       */
+int _sqrt_helper(int n, int guess)
+{
+		if (guess * guess == n)
+		{
+					return (guess);
+		}
+
+			if (guess * guess > n)
+			{
+						return (-1);
+			}
+
+				return (_sqrt_helper(n, guess + 1));
+}
+
+/**
+ *  * _sqrt_recursion - returns the natural square root of a number
+ *   * @n: number to find square root of
+ *    *
+ *     * Return: natural square root of n, or -1 if not found
+ *      */
+int _sqrt_recursion(int n)
+{
+		if (n < 0)
+		{
+					return (-1);
+		}
+
+			if (n == 0 || n == 1)
+			{
+						return (n);
+			}
+
+				return (_sqrt_helper(n, 1));
+}#include "main.h"
+
+/**
+ *  * _sqrt_helper - helper function to find square root using binary search
+ *   * @n: number to find square root of
+ *    * @guess: current guess for square root
+ *     *
+ *      * Return: natural square root of n, or -1 if not found
+ *       */
+int _sqrt_helper(int n, int guess)
+{
+		if (guess * guess == n)
+		{
+					return (guess);
+		}
+
+			if (guess * guess > n)
+			{
+						return (-1);
+			}
+
+				return (_sqrt_helper(n, guess + 1));
+}
+
+/**
+ *  * _sqrt_recursion - returns the natural square root of a number
+ *   * @n: number to find square root of
+ *    *
+ *     * Return: natural square root of n, or -1 if not found
+ *      */
+int _sqrt_recursion(int n)
+{
+		if (n < 0)
+		{
+					return (-1);
+		}
+
+			if (n == 0 || n == 1)
+			{
+						return (n);
+			}
+
+				return (_sqrt_helper(n, 1));
+}#include "main.h"
+
+/**
+ *  * _sqrt_helper - helper function to find square root using binary search
+ *   * @n: number to find square root of
+ *    * @guess: current guess for square root
+ *     *
+ *      * Return: natural square root of n, or -1 if not found
+ *       */
+int _sqrt_helper(int n, int guess)
+{
+		if (guess * guess == n)
+		{
+					return (guess);
+		}
+
+			if (guess * guess > n)
+			{
+						return (-1);
+			}
+
+				return (_sqrt_helper(n, guess + 1));
+}
+
+/**
+ *  * _sqrt_recursion - returns the natural square root of a number
+ *   * @n: number to find square root of
+ *    *
+ *     * Return: natural square root of n, or -1 if not found
+ *      */
+int _sqrt_recursion(int n)
+{
+		if (n < 0)
+		{
+					return (-1);
+		}
+
+			if (n == 0 || n == 1)
+			{
+						return (n);
+			}
+
+				return (_sqrt_helper(n, 1));
+}#include "main.h"
+
+/**
+ *  * _sqrt_helper - helper function to find square root using binary search
+ *   * @n: number to find square root of
+ *    * @guess: current guess for square root
+ *     *
+ *      * Return: natural square root of n, or -1 if not found
+ *       */
+int _sqrt_helper(int n, int guess)
+{
+		if (guess * guess == n)
+		{
+					return (guess);
+		}
+
+			if (guess * guess > n)
+			{
+						return (-1);
+			}
+
+				return (_sqrt_helper(n, guess + 1));
+}
+
+/**
+ *  * _sqrt_recursion - returns the natural square root of a number
+ *   * @n: number to find square root of
+ *    *
+ *     * Return: natural square root of n, or -1 if not found
+ *      */
+int _sqrt_recursion(int n)
+{
+		if (n < 0)
+		{
+					return (-1);
+		}
+
+			if (n == 0 || n == 1)
+			{
+						return (n);
+			}
+
+				return (_sqrt_helper(n, 1));
+}#include "main.h"
+
+/**
+ *  * _sqrt_helper - helper function to find square root using binary search
+ *   * @n: number to find square root of
+ *    * @guess: current guess for square root
+ *     *
+ *      * Return: natural square root of n, or -1 if not found
+ *       */
+int _sqrt_helper(int n, int guess)
+{
+		if (guess * guess == n)
+		{
+					return (guess);
+		}
+
+			if (guess * guess > n)
+			{
+						return (-1);
+			}
+
+				return (_sqrt_helper(n, guess + 1));
+}
+
+/**
+ *  * _sqrt_recursion - returns the natural square root of a number
+ *   * @n: number to find square root of
+ *    *
+ *     * Return: natural square root of n, or -1 if not found
+ *      */
+int _sqrt_recursion(int n)
+{
+		if (n < 0)
+		{
+					return (-1);
+		}
+
+			if (n == 0 || n == 1)
+			{
+						return (n);
+			}
+
+				return (_sqrt_helper(n, 1));
+}#include "main.h"
+
+/**
+ *  * _sqrt_helper - helper function to find square root using binary search
+ *   * @n: number to find square root of
+ *    * @guess: current guess for square root
+ *     *
+ *      * Return: natural square root of n, or -1 if not found
+ *       */
+int _sqrt_helper(int n, int guess)
+{
+		if (guess * guess == n)
+		{
+					return (guess);
+		}
+
+			if (guess * guess > n)
+			{
+						return (-1);
+			}
+
+				return (_sqrt_helper(n, guess + 1));
+}
+
+/**
+ *  * _sqrt_recursion - returns the natural square root of a number
+ *   * @n: number to find square root of
+ *    *
+ *     * Return: natural square root of n, or -1 if not found
+ *      */
+int _sqrt_recursion(int n)
+{
+		if (n < 0)
+		{
+					return (-1);
+		}
+
+			if (n == 0 || n == 1)
+			{
+						return (n);
+			}
+
+				return (_sqrt_helper(n, 1));
+}#include "main.h"
+
+/**
+ *  * _sqrt_helper - helper function to find square root using binary search
+ *   * @n: number to find square root of
+ *    * @guess: current guess for square root
+ *     *
+ *      * Return: natural square root of n, or -1 if not found
+ *       */
+int _sqrt_helper(int n, int guess)
+{
+		if (guess * guess == n)
+		{
+					return (guess);
+		}
+
+			if (guess * guess > n)
+			{
+						return (-1);
+			}
+
+				return (_sqrt_helper(n, guess + 1));
+}
+
+/**
+ *  * _sqrt_recursion - returns the natural square root of a number
+ *   * @n: number to find square root of
+ *    *
+ *     * Return: natural square root of n, or -1 if not found
+ *      */
+int _sqrt_recursion(int n)
+{
+		if (n < 0)
+		{
+					return (-1);
+		}
+
+			if (n == 0 || n == 1)
+			{
+						return (n);
+			}
+
+				return (_sqrt_helper(n, 1));
+}
