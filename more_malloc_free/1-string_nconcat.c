5d7a546bf54c90ec03b20a2919820879e9bf2574
csrama <ramadafer1@gmail.com>
2025-11-04 09:45:12 +0000

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,71 +1,46 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * _strlen - calculates the length of a string
- * @s: the string to measure
+ * string_nconcat - Concatenates two strings
+ * @s1: The first string
+ * @s2: The second string
+ * @n: The number of bytes from s2 to concatenate
  *
- * Return: length of the string
- */
-unsigned int _strlen(char *s)
-{
-	unsigned int len = 0;
-
-	if (s == NULL)
-		return (0);
-
-	while (s[len] != '\0')
-		len++;
-
-	return (len);
-}
-
-/**
- * string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: number of bytes from s2 to concatenate
- *
- * Return: pointer to newly allocated concatenated string
- *         NULL if allocation fails
+ * Return: Pointer to the newly allocated memory containing s1,
+ *         followed by the first n bytes of s2, and null terminated.
+ *         If the function fails, return NULL.
+ *         If NULL is passed, treat it as an empty string.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1, len2, total_len, i, j;
-	char *result;
+	char *ptr;
+	unsigned int len1 = 0, len2 = 0, i, j;
 
-	/* Handle NULL strings by treating them as empty strings */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	/* Get lengths of strings */
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
 
-	/* If n >= length of s2, use entire s2 */
 	if (n >= len2)
 		n = len2;
 
-	/* Calculate total length needed (s1 + n bytes of s2 + null terminator) */
-	total_len = len1 + n + 1;
-
-	/* Allocate memory */
-	result = malloc(total_len);
-	if (result == NULL)
+	ptr = malloc(sizeof(char) * (len1 + n + 1));
+	if (ptr == NULL)
 		return (NULL);
 
-	/* Copy s1 to result */
 	for (i = 0; i < len1; i++)
-		result[i] = s1[i];
+		ptr[i] = s1[i];
 
-	/* Copy first n bytes of s2 */
 	for (j = 0; j < n; j++)
-		result[i + j] = s2[j];
+		ptr[i + j] = s2[j];
 
-	/* Add null terminator */
-	result[i + j] = '\0';
+	ptr[i + j] = '\0';
 
-	return (result);
+	return (ptr);
 }
