72e23da184c122e74479483f24b93c8ad0164346
csrama <ramadafer1@gmail.com>
2025-12-01 19:29:15 +0000

    a function that adds a new node at the end of a dlistint_t list.

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,42 +2,41 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - Adds a new node at the end of a dlistint_t list
- * @head: Double pointer to the head of the list
- * @n: Integer value to store in the new node
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * @head: double pointer to the head of the list
+ * @n: integer value to store in the new node
  *
- * Return: Address of the new element, or NULL if it failed
+ * Return: address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
-    dlistint_t *current;
+    dlistint_t *new, *tmp;
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
+    if (!head)
+        return NULL;
 
-    /* Initialize the new node */
-    new_node->n = n;
-    new_node->next = NULL;
-    new_node->prev = NULL;
+    new = malloc(sizeof(*new));
+    if (!new)
+        return NULL;
 
-    /* If the list is empty, the new node becomes the head */
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
+    new->n = n;
+    new->next = NULL;
+    new->prev = NULL;
+
+    /* If list is empty, new node becomes head */
+    if (!*head) {
+        *head = new;
+        return new;
     }
 
-    /* Traverse to the end of the list */
-    current = *head;
-    while (current->next != NULL)
-        current = current->next;
+    /* Traverse to the last node */
+    tmp = *head;
+    while (tmp->next)
+        tmp = tmp->next;
 
-    /* Add the new node at the end */
-    current->next = new_node;
-    new_node->prev = current;
+    /* Link new node at the end */
+    tmp->next = new;
+    new->prev = tmp;
 
-    return (new_node);
+    return new;
 }
